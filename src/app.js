0cda047240a883fccad12e7e84a2aa65ba5da7a4
HugoMProject <hugomoyano1996@gmail.com>
2022-07-18 21:17:27 -0300

    aprendiendo a reutilizar vistas con EJS

--- src/app.js
+++ src/app.js
@@ -3,12 +3,24 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/view'))
 
-// app.use(express.static(path.join(__dirname, 'public')))
 // Resgistrar como publica/static la carpeta public
 app.use(express.static('public'));
 
 app.use('/desktop',express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.render('index',{
+        usuario: {
+            name:'hugo',
+            email:'hugo@gmail.com'
+        }
+    });
+})
+
+
 
 app.get("/",(req, res)=>{
     res.send('Bienvenido a la pagina Principal')
